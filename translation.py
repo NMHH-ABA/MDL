dd3fd45c67826062c9a8aabadae0b5f425952fdd
NMHH-ABA <NMHH.ABA@gmail.com>
2020-10-20 12:22:02 +0330

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,20 @@
 class Translation(object):
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
-Source Code: https://github.com/SpEcHiDe/AnyDLBot
-© @AnyDLBot"""
+    START_TEXT = """با این ربات میتونید ویدیو های ایران اینترنشنال رو دانلود کنید
+    کافیه آدرس صفحه موردنظر رو بفرستید 
+© @BachehayeManoto"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "This is an @AnyDLBot clone created using the Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nStart @AnyDLBot to know Paid Plans!"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    FORMAT_SELECTION = "کیفیت موردنظر رو انتخاب کنید"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "Downloading..."
-    UPLOAD_START = "Uploading..."
+    DOWNLOAD_START = "در حال دانلود..."
+    UPLOAD_START = "در حال آپلود..."
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
+    RCHD_TG_API_LIMIT = "دانلود شده در {} ثانیه.\nحجم ویدیو: {}\nمتاسفانه بدلیل محدودیت تلگرام امکان آپلود ویدیو با حجم بیشتر از 2 گیگ نیست."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "دانلود برنامه های من و تو @BachehayeManoto"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "دانلود شده در {} ثانیه. \nدانلود برنامه های من و تو @BachehayeManoto \nآپلود شده در {} ثانیه."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
